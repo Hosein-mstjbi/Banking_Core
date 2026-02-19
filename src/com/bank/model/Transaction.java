0df165b3b5bf7c1557a404d84f2bf5f1cf2cdb9b
Hosein Mostajabi <Hosein.mostajabii@gmail.com>
2026-02-19 12:39:49 +0330

    implement Transaction model

--- src/com/bank/model/Transaction.java
+++ src/com/bank/model/Transaction.java
@@ -1,4 +1,22 @@
 package com.bank.model;
 
-public class Transaction {
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+public class Transaction implements Serializable {
+
+    private String type;
+    private double amount;
+    private LocalDateTime timestamp;
+
+    public Transaction(String type, double amount) {
+        this.type = type;
+        this.amount = amount;
+        this.timestamp = LocalDateTime.now();
+    }
+
+    @Override
+    public String toString() {
+        return type + " | " + amount + " | " + timestamp;
+    }
 }
